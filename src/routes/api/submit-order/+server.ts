d37a8e8346b49b5791fba122c97ff1a07567b1ad
Laestry <georg3600@gmail.com>
2025-08-28 02:10:11 +0700

    fix submit

--- src/routes/api/submit-order/+server.ts
+++ src/routes/api/submit-order/+server.ts
@@ -3,11 +3,17 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
     const payload = await request.json();
-    const res = await fetch('https://enos.is/api/c/saq/v1/create-external-sales-order', {
-        // const res = await fetch('http://localhost:7777/api/custom/saq/v1/create-external-sales-order', {        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(payload)
-    });
+    let res;
+    try {
+        res = await fetch('https://enos.is/api/c/saq/v1/create-external-sales-order', {
+            // const res = await fetch('http://localhost:7777/api/custom/saq/v1/create-external-sales-order', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+        });
+    } catch (e) {
+        console.error('// src/routes/api/submit-order/+server.ts', e);
+    }
 
     if (!res.ok) {
         const errorBody = await res.text();
