0a7128f48d69a6607bcd99c0dddcc614719ba3a4
Laestry <georg3600@gmail.com>
2025-10-29 09:10:30 +0700

    return images

--- src/lib/utils/images.ts
+++ src/lib/utils/images.ts
@@ -8,7 +8,8 @@ export function getRandomNumber(product) {
 }
 export function getVinsImage(product) {
     if (product.main_image_file) return ALCOHOL_BASE_PATH + product.main_image_file;
-    return '/defaultImages/default-alcohol.png';
+    const n = getRandomNumber(product);
+    return images[n % images.length]!;
 }
 
 export function getVinImage(product, order) {
@@ -22,5 +23,12 @@ export function getVinImage(product, order) {
         return ALCOHOL_BASE_PATH + images[order].files.uuid + '/' + images[order].files.file_name;
     }
 
-    return '/defaultImages/default-alcohol.png';
+    switch (order) {
+        case 0:
+            return '/images/example_wines/SHOP PAGE/Product Shot - stack.png';
+        case 1:
+            return '/images/example_wines/SHOP PAGE/9x16 Product Shot - stack.png';
+        case 2:
+            return '/images/example_wines/SHOP PAGE/In-situ Product Shot - stack.png';
+    }
 }
